183a792b1416248eeccfd2c0b982e67e3f6f2fcc
Isaac <bennisaac24@gmail.com>
2024-11-30 12:40:01 +0300

    scope chain and function expression labs done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun (activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+saturdayFun();
+console.log(saturdayFun("bathe my dog"));
+
+function mondayWork (task = 'go to the office'){
+    return `This Monday, I will ${task}.`
+}
+
+mondayWork();
+console.log(mondayWork("work from home"));
+
+
+function wrapAdjective(para = "*"){
+    const name = function(adj = "special"){
+        return `You are ${para}${adj}${para}!`;
+    };
+    return name;
+}
+
+wrapAdjective('*') ("special");
+
+
